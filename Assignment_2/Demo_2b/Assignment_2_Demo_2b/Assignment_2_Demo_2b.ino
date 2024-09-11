ee5b14623ef86eb87aba13e39e39aa97b571a7c3
Luca Ciancanelli <luca.ciancanelli1785@gmail.com>
2024-09-11 14:13:03 -0600

    Assignment 2 directory setup

---
+++ Assignment_2/Demo_2b/Assignment_2_Demo_2b/Assignment_2_Demo_2b.ino
@@ -0,0 +1,13 @@
+// Name: Assignment_2_Demo_2_B
+// Author: Luca Ciancanelli
+// EENG350 - Section B
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
