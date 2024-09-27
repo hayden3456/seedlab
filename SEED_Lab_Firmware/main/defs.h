ed54d4db80b167d413a480fc5269dd0ea2bb6ba9
dbowling212 <david_bowling@mines.edu>
2024-09-27 14:07:13 -0600

    Update defs.h

--- SEED_Lab_Firmware/main/defs.h
+++ SEED_Lab_Firmware/main/defs.h
@@ -11,4 +11,13 @@
 #define RIGHT_MOTOR_INPUT_A 3
 #define RIGHT_MOTOR_INPUT_B 6
 
+// Controls constants
+#define Kp = 0.2
+#define K = 1.9
+#define sigma = 16
+#define Kp_pos = 3.4
+#define Ki_pos = 2
+
+#define desired_Ts_ms = 10
+
 #endif
