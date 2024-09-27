a341b9dfcb0efc97a16e8a23d10a0e01da73b50f
dbowling212 <david_bowling@mines.edu>
2024-09-27 12:58:47 -0600

    Update SEED_Lab_PID_controller.h

--- SEED_Lab_Firmware/main/SEED_Lab_PID_controller.h
+++ SEED_Lab_Firmware/main/SEED_Lab_PID_controller.h
@@ -10,8 +10,8 @@ class PID_control{
 
     public:
         float get_des_vel(int motor_num);
-        void set_right_vel(long *desired_pos, float measured_pos = get_wheel_pos(0), float *desired_right_vel);
-        void set_right_vel(long *desired_pos, float measured_pos = get_wheel_pos(1), float *desired_left_vel);
+        void set_right_vel(long *desired_pos, float measured_pos = get_wheel_pos(1), float *desired_right_vel);
+        void set_right_vel(long *desired_pos, float measured_pos = get_wheel_pos(0), float *desired_left_vel);
 
 };
 
